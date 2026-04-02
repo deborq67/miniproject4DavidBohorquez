a381130de3d934b7bf1d0658a2e6cf48a59f013b
deborq67 <debohorquez@mail.fhsu.edu>
2026-04-01 23:05:27 -0500

    Figured out history functionality. Need to link it to navbar.

--- organellesearch/results/urls.py
+++ organellesearch/results/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import search_view
+from .views import search_view, history_view
 
 urlpatterns = [
     path("", search_view, name="search"),
+    path("history/", history_view, name="history"),
 ]
--- organellesearch/results/views.py
+++ organellesearch/results/views.py
@@ -2,17 +2,13 @@ from django.shortcuts import render
 from .forms import SearchForm
 from .organelle_ambiguity import initiate_search
 from .models import SearchResult, SearchHistory
+from django.utils import timezone
 
 def search_view(request):
     if request.method == "POST":
         form = SearchForm(request.POST)
-        print(form.errors)
         if form.is_valid():
-            df, total_records = initiate_search(
-                form.cleaned_data["search_term"],
-                request.user.email
-            )
-
+            df, total_records = initiate_search(form.cleaned_data["search_term"],request.user.email)
             # Save each row as a modal row in SearchResult.
             result_objects = []
             for _, row in df.iterrows():
@@ -26,12 +22,12 @@ def search_view(request):
                 result_objects.append(result)
 
             # Save the search history as a modal row in SearchHistory.
-            history = SearchHistory.objects.create(
+            SearchHistory.objects.create(
                 user=request.user,
                 search_term=form.cleaned_data["search_term"],
-                total_records=total_records
+                total_records=total_records,
+                searched_at=timezone.now(),
             )
-            history.results.set(result_objects)
 
             return render(request, "search/results.html", {
                 "results": result_objects,
@@ -41,3 +37,7 @@ def search_view(request):
     else:
         form = SearchForm()
     return render(request, "index.html", {"form": form})
+
+def history_view(request):
+    history = SearchHistory.objects.filter(user=request.user).order_by("-searched_at")
+    return render(request, "search/history.html", {"history": history})
