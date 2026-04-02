42d3c1f6b038f37a4e3b2925cbef1ff79c0b00d6
deborq67 <debohorquez@mail.fhsu.edu>
2026-04-01 23:33:01 -0500

    Added history functionality to navbar.

--- organellesearch/results/urls.py
+++ organellesearch/results/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import search_view, history_view
+from .views import search_view
 
 urlpatterns = [
     path("", search_view, name="search"),
-    path("history/", history_view, name="history"),
 ]
